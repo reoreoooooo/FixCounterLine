4e0d8daf4d3b4541a0d1f0e80ef8611a95b41342
Reo Kuchida <kuchidareo@gmail.com>
2022-02-05 17:22:03 +0900

    Add posting imageData function

--- test.js
+++ test.js
@@ -3,23 +3,39 @@ import http from "http";
 import * as fs from "fs";
 import path from "path";
 import express from "express";
+import bodyParser from "body-parser";
+import formData from "express-form-data";
 import dirname from 'path';
 
 const __dirname = path.dirname(new URL(import.meta.url).pathname).slice(1);
 
 let app = express();
+
 app.use(express.static(__dirname));
 
-app.get('/', function (req, res) {
+// Express-Form-Dataの初期化
+const updir = path.dirname(__dirname).replace(/\\/g, "/") + "/tmp"; 
+app.use(formData.parse({uploadDir:updir, autoClean:true, maxFieldsSize:"50mb"}));
+
+
+
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.post('/imgFormed', (req, res) => {
+  uploadImg(req.body["imgData"], req.body["coresCsvData"]);
+
+  res.send("Received POST Data!");
+});
+
 app.listen(5000, function () {
-  console.log('Example app listening on port 5000!');
+  console.log('Your local server listening on port 5000!');
 });
 
 
-function uploadImg(imgData) {
+function uploadImg(imgData, coresCsvData) {
+
     const url = "https://mds.meatimage.jp/api/beef/image/__auto__/input.jpg?";
     const token = "36a55dc836176b49562c0f3a4b0d2f20709af9b9";
 
@@ -32,10 +48,16 @@ function uploadImg(imgData) {
       body: imgData
     }
 
+    // [0]: Owner, [1]: TakenAt, [2]: BarcodeNo, [3]: BodyNo
+    let barcode = coresCsvData[2];
+    let bodyno = coresCsvData[3];
+    let takenat = coresCsvData[1];
+    let owner = coresCsvData[0];
+
     let p = {
-      "barcode_no" : "1",
-      "body_no" : "1",
-      "taken_at" : "2022-01-11T23%3A50%3A00%2B09%3A00",
+      "barcode_no" : barcode,
+      "body_no" : bodyno,
+      "taken_at" : takenat,
       "owner" : "kuchida",
       "do_analysis" : "True",
       "ppmm" : "10"
